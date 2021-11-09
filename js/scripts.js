09ddf595550aeae5ed5d3d5dca08b88d38b1d1c4
254jack <taixonj@gmail.com>
2021-11-09 23:54:38 +0300

    add js changes to gh-pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,35 @@
+//create a toggle effect function on pruduct paragraph and image
+
 $(document).ready(function() {
-  $("#product").click(function() {
-    $("#product-text").toggle();
-    $("#products-img").toggle();
-  });
+    $("#product").click(function() {
+        $("#product-text").toggle();
+        $("#products-img").toggle();
+    });
 });
+
+
+//create a function that toggle between development text 
 $(document).ready(function() {
-  $("#design").click(function() {
-    $("#design-text").toggle();
-    $("#design-img").toggle();
-  });
+    $("#design").click(function() {
+        $("#design-text").toggle();
+        $("#design-img").toggle();
+    });
 });
+
+
+//create a toggle effect for development text
 $(document).ready(function() {
-  $("#development").click(function() {
-    $("#development-text").toggle();
-    $("#development-img").toggle();
-  });
+    $("#development").click(function() {
+        $("#development-text").toggle();
+        $("#development-img").toggle();
+    });
 });
-$(document).ready(function(){
-  $("form").submit(function(){
-    alert("Hello we have received your message and we are working on it.Thank you for keeping in touch with us.")
-  });
+
+
+//create a function that gives a feedback after the user submits the details
+$(document).ready(function() {
+    $("form").submit(function() {
+        var name = $("#name").val();
+        alert("Hello " + name + " we have received your message and we are working on it.Thank you for keeping in touch with us.")
+    });
 });
